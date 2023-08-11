43971b176ef6e1a604436c6968dfd8dcc94b7e73
surafel58 <surafelsentayehu58@gmail.com>
2023-08-11 13:31:09 +0300

    Add food recipe task

---
+++ Food Recipe App Task/food-recipe-app/public/index.js
@@ -0,0 +1,11 @@
+burger = document.querySelector("#burger"); 
+menu = document.querySelector("#menu");
+
+burger.addEventListener('click', () => {
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden');
+    }
+    else{
+        menu.classList.add('hidden');
+    } 
+});
