e0b22b84657f7d3cd82118bdbc63fc4429ad276f
surafel58 <surafelsentayehu58@gmail.com>
2023-08-14 22:07:53 +0300

    Edit Blog app task

--- Blog app Task/blog-app/app/blogs/[id]/edit/page.jsx
+++ Blog app Task/blog-app/app/blogs/[id]/edit/page.jsx
@@ -1,51 +1,96 @@
 "use client";
-import {useParams, useRouter  } from 'next/navigation';
+import { useRouter, useSearchParams } from "next/navigation";
 import styles from "./editform.module.css";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const fetchBlog = async (id) => {
-  const response = await fetch('http://localhost:8000/blogs/' + id , {cache: "no-cache"});
+  const response = await fetch("http://localhost:8000/blogs/" + id, {
+    cache: "no-cache",
+  });
   const data = await response.json();
-  return data
-}
+  return data;
+};
 
-const EdtForm = async() => {
+const editBlog = async (data, id, router) => {
+  fetch("http://localhost:8000/blogs/" + id, {
+    method: "PUT",
+    cache: "no-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
 
+  router.push("/");
+};
 
-  const {current_title, current_content} = await fetchBlog(params.id);
+const EdtForm = async () => {
+  const params = useSearchParams();
+  const id = params.get("id");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const router = useRouter();
+  const titleRef = useRef();
 
-  const [title, setTitle] = useState(current_title);
-  const [content, setContent] = useState(current_content);
+  useEffect(() => {
+    let blog;
+    fetchBlog(id).then((data) => {
+      blog = data;
+      setTitle(blog.title);
+      setContent(blog.content);
+    });
+  }, []);
 
-
-  const editBlog = async (data) => {
-    fetch('http://localhost:8000/blogs/'+params.id, {
-     method: "PUT",
-     cache: "no-cache",
-     headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify(data)
-      }
+  return (
+    <form
+      method="put"
+      className={styles.blog_form}
+      onSubmit={async (e) => {
+        e.preventDefault();
+        await editBlog(
+          {
+            id: new Date().getMilliseconds().toString(),
+            title,
+            content,
+            date:
+              new Date().getDate().toString() +
+              "/" +
+              new Date().getMonth().toString() +
+              "/" +
+              new Date().getFullYear(),
+          },
+          id,
+          router
+        );
+      }}
+    >
+      <div className={styles.blog_form_inputs}>
+        <label>Blog title: </label>{" "}
+        <input
+          ref={titleRef}
+          required
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <label>Blog body: </label>{" "}
+        <textarea
+          required
+          cols={10}
+          rows={10}
+          name="content"
+          value={content}
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit">Edit Blog</button>
+    </form>
   );
+};
 
-    router.push('/');
-
-  }
-
-    return ( 
-        <form method="put" className={styles.blog_form} onSubmit={async (e) => {
-          e.preventDefault();
-          await editBlog({id: new Date().getMilliseconds().toString(), title, content, date: new Date().getDate().toString() + "/" + new Date().getMonth().toString() + "/" + new Date().getFullYear()});
-        }}> 
-            <div className={styles.blog_form_inputs}>
-                <label>Blog title: </label> <input required type="text" name="title" value={title} onChange={(e) => {setTitle(e.target.value)}}/>
-                <label>Blog body: </label> <textarea required cols={10} rows={10} name="content" value={content} onChange={(e) => {setContent(e.target.value)}}/>
-                {/* <span hidden name="date">{new Date().getDate().toString() + "/" + new Date().getMonth().toString() + "/" + new Date().getFullYear()}</span> */}
-            </div>
-            <button type="submit">Edit Blog</button>
-        </form>
-     );
-}
- 
 export default EdtForm;
--- Blog app Task/blog-app/app/blogs/[id]/page.jsx
+++ Blog app Task/blog-app/app/blogs/[id]/page.jsx
@@ -1,42 +1,54 @@
 "use client";
+import Link from "next/link";
 import styles from "./blogdetail.module.css";
-import { usePathname, useRouter, useParams  } from 'next/navigation';
+import { usePathname, useRouter, useParams } from "next/navigation";
 
 const fetchBlog = async (id) => {
-    const response = await fetch('http://localhost:8000/blogs/' + id, {cache: "no-cache"});
-    const data = await response.json();
-    return data
-}
+  const response = await fetch("http://localhost:8000/blogs/" + id, {
+    cache: "no-cache",
+  });
+  const data = await response.json();
+  return data;
+};
 
 const deleteBlogs = async (id) => {
-    const response = await fetch('http://localhost:8000/blogs/' + id, {method: "delete"});
-    return response.status;
-}
+  const response = await fetch("http://localhost:8000/blogs/" + id, {
+    method: "delete",
+  });
+  return response.status;
+};
 
-const BlogDetail = async ({params}) => {
-    
-    const router = useRouter();
-    const blog = await fetchBlog(params.id);
+const BlogDetail = async ({ params }) => {
+  const router = useRouter();
+  const blog = await fetchBlog(params.id);
 
+  return (
+    <div className={styles.blog_detail}>
+      {blog && (
+        <>
+          <h2>{blog.title}</h2>
+          <p>Date: {blog.date}</p>
+          <p>{blog.content}</p>
+          <Link
+            href={{
+              pathname: `${params.id}/edit`,
+              query: { id: params.id },
+            }}
+          >
+            <button>Edit</button>
+          </Link>
+          <button
+            onClick={(e) => {
+              deleteBlogs(params.id);
+              router.replace("/");
+            }}
+          >
+            Delete
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
 
-    return ( 
-        
-        <div className={styles.blog_detail}>
-            {blog && 
-            <>
-                <h2>{blog.title}</h2>
-                <p>Date: {blog.date}</p>
-                <p>{blog.content}</p>
-                <button onClick={(e) => {
-                    router.push(`${params.id}/edit`);
-                }}>Edit</button>
-                <button onClick={(e) => {
-                    deleteBlogs(params.id);
-                    router.replace('/');
-                    }}>Delete</button>
-            </>}  
-        </div>
-        );
-}
- 
 export default BlogDetail;
